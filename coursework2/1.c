5bbe4eae8bd7ce55a9168337a6cfba6c9bed8846
konodiodaa <44199257+konodiodaa@users.noreply.github.com>
2018-12-08 21:29:36 +0800

    Add files via upload

---
+++ coursework2/1.c
@@ -0,0 +1,185 @@
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+
+// tree data structure
+
+struct qnode {
+  int level;
+  double xy[2];
+  struct qnode *child[4];
+};
+typedef struct qnode Node;
+
+// function definitions
+
+Node *makeNode( double x, double y, int level );
+void makeChildren( Node *parent );
+void writeTree( Node *head );
+void writeNode( FILE *fp, Node *node );
+void printOut( FILE *fp, Node *node );
+void growTree(Node *current);
+void removeChild(Node *parent);
+// main
+
+int main( int argc, char **argv ) {
+
+  Node *head;
+
+  // make the head node
+  head = makeNode( 0.0,0.0, 0 );
+
+  // make a tree
+  makeChildren( head );
+  makeChildren(head->child[0]);
+  growTree(head);
+  removeChild(head->child[2]);
+  // print the tree for Gnuplot
+	writeTree( head );
+
+  return 0;
+}
+
+// make a node at given location (x,y) and level
+
+Node *makeNode( double x, double y, int level ) {
+
+  int i;
+
+  Node *node = (Node *)malloc(sizeof(Node));
+
+  node->level = level;
+
+  node->xy[0] = x;
+  node->xy[1] = y;
+
+  for( i=0;i<4;++i )
+    node->child[i] = NULL;
+
+  return node;
+}
+
+// split a leaf nodes into 4 children
+
+void makeChildren( Node *parent ) {
+
+  double x = parent->xy[0];
+  double y = parent->xy[1];
+
+  int level = parent->level;
+
+  double hChild = pow(2.0,-(level+1));
+
+  parent->child[0] = makeNode( x,y, level+1 );
+  parent->child[1] = makeNode( x+hChild,y, level+1 );
+  parent->child[2] = makeNode( x+hChild,y+hChild, level+1 );
+  parent->child[3] = makeNode( x,y+hChild, level+1 );
+
+  return;
+}
+
+//grow the Tree with a full level
+
+void growTree(Node *parent)
+{
+  int i;
+  if(parent->child[0]==NULL)
+  {
+      makeChildren(parent);
+  }
+  else
+  {
+      for(i=0;i<4;i++)
+      {
+          growTree(parent->child[i]);
+      }
+  }
+}
+
+//remove the children of a node
+
+void removeChild(Node *parent)
+{
+  int i;
+  if(parent->child[0]==NULL)
+  {
+    printf("This node doesn`t have children now");
+  }
+  else if(parent->child[0]!=NULL && parent->child[0]->child[0]!=NULL)
+  {
+    printf("Error, this node`s children are not leaf node!!!");
+  }
+  else
+  {
+    for(i=0;i<4;i++)
+    {
+      parent->child[i]=NULL;
+      free(parent->child[i]);
+    }
+  }
+}
+
+//release all the storage
+
+void destroyTree(Node *head)
+{
+  int i;
+  if(head->child[0]==NULL)
+  {
+    free(head);
+  }
+  else
+  {
+    for(i=0;i<4;i++)
+    {
+      destroyTree(head->child[i]);
+    }
+  }
+}
+
+// write out the tree to file 'quad.out'
+
+void writeTree( Node *head ) {
+
+  FILE *fp = fopen("quad.out","w");
+
+  writeNode(fp,head);
+
+  fclose(fp);
+
+  return;
+}
+
+// recursively visit the leaf nodes
+
+void writeNode( FILE *fp, Node *node ) {
+
+  int i;
+
+  if( node->child[0] == NULL )
+    printOut( fp, node );
+  else {
+    for ( i=0; i<4; ++i ) {
+      writeNode( fp, node->child[i] );
+    }
+  }
+  return;
+}
+
+// write out the (x,y) corners of the node
+
+void printOut( FILE *fp, Node *node ) {
+  double x = node->xy[0];
+  double y = node->xy[1];
+  int level = node->level;
+  double h = pow(2.0,-level);
+
+  fprintf(fp, " %g %g\n",x,y);
+  fprintf(fp, " %g %g\n",x+h,y);
+  fprintf(fp, " %g %g\n",x+h,y+h);
+  fprintf(fp, " %g %g\n",x,y+h);
+  fprintf(fp, " %g %g\n\n",x,y);
+
+  return;
+}
